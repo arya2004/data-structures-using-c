98222d7c659aad6a711a050d9d3cf5a7181b67f5
arya2004 <arya20j4@gmail.com>
2023-05-21 22:45:09 +0530

    l

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include<stdbool.h>
 //#include "stack_array.h"
 //#include "doublelinkedlist.h"
 //#include "queue_array.h"
@@ -15,27 +16,8 @@
 //#include "merge_sort.h"
 //#include "search.h"
 //#include "circularlinkedlist.h"
-#include "sort.h"
-// int q ()
-// {
-//   int a[10] = {11,22,33,44,55,67,77,88,99, 100};
-//   int s = 10;
-//   int num = 101;
-//   int mid = 
-//   printf("\n%i", binarySearch(a,10,0,10,(0+9)/2, num));
 
-//   return 0;
-// }
-int main ()
-{ int pp[10] = {4,6,5,9,1,0,2,8,3,7};
-int a = 10;
-
-  int *p = insertionSort(pp, a);
-  for (int i = 0; i < 10; i++)
-  {
-    printf("\n%i", p[i]);
-  }
-  
-  
-  return 0;
+int main()
+{
+   
 }
